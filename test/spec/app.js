6749215d9ffaba2afb319ba219d44ac794e56752
Daniel Stern <daniel@danielstern.ca>
2015-05-15 22:13:15 -0400

    gulp serve-test working!!!

--- test/spec/app.js
+++ test/spec/app.js
@@ -1,6 +1,11 @@
 describe("the contact list",function(){
 	angular.module('contactList.test',['AddressBook']);
+	
+	it('should pass',function(){
+		assert.isTrue(true);			
+	});
+	
 	it('should return an array of contacts',function(){
 		assert.equal(1,2);			
-	})
+	});
 })
