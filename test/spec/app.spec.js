37c129e16f4a51380f82fd5b260f9dfd2c9ef25a
Daniel Stern <daniel@danielstern.ca>
2015-05-16 08:37:10 -0400

    add test for controller

--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -1,6 +1,7 @@
 var assert = chai.assert;
+var expect = chai.expect;
 
-describe("the contact list",function(){
+describe("The Contact Service",function(){
 	beforeEach(function(){
 		module('AddressBook');
 		
@@ -12,4 +13,23 @@ describe("the contact list",function(){
 			assert.isArray(contacts);
 		}
 	));
+});
+
+describe("The Contact List Controller",function(){
+	beforeEach(function(){
+		module('AddressBook');	
+	});
+	
+	it('should get a copy of the contact list on startup and store it in $scope',
+		inject(function($rootScope,$controller,contactService){
+			/* Create a new scope that we can assign as the controllers scope. By keeping a reference to it, we can run tests. */
+			var $scope = $rootScope.$new();
+			var contactListController = $controller("ContactList",{$scope:$scope});
+			var contacts = $scope.contacts;
+			expect(contacts).to.be.an('array');
+			expect(contacts).to.deep.equal(contactService.getContacts());
+		}
+	));
+
+
 })
