8364bf985434aeb0e57ce8aa079388774c1e58a5
Daniel Stern <daniel@danielstern.ca>
2015-05-15 21:20:13 -0400

    scaffold browser sync and index file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,37 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
+/* build our app to the dist folder */
 gulp.task('default',function(){
 	console.log("King to B6.");
 });
 
+/* run all tests */
 gulp.task('test',function(){
 	console.log("Running tests.");
+});
+
+/* serve the app */
+gulp.task('serve', function () {
+  browserSync({
+    notify: false,
+    port: 8080,
+    server: {
+      baseDir: ['app'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+
+  gulp.watch([
+    'app/*.html',
+    'app/scripts/**/*.js',
+  ]).on('change', reload);
+});
+
+/* serve the output of the test runner (for debugging) */
+gulp.task('serve-test',function(){
+
 })
