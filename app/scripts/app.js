a0ae151c51120bfee35aba14c2b21b27701c7d57
Daniel Stern <daniel@danielstern.ca>
2015-05-15 21:29:59 -0400

    add first service

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,3 +2,14 @@ angular.module('AddressBook',[])
 .run(function(){
 	console.log("Address Book has initialized.");
 })
+.service("contactService",function(){
+	var contacts = [];
+	
+	function getContacts(){
+		return contacts;
+	};
+	
+	return {
+		getContacts:getContacts
+	}
+})
