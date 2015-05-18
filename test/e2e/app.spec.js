35d2079e4bda014140c46227ba918778e364c2ad
Daniel Stern <daniel@danielstern.ca>
2015-05-18 19:58:50 -0400

    adding protractor tests. working on it but its slow stuff.

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -1,24 +1,40 @@
 describe('adding a contact', function() {
-  it('should add the appropriate contact', function(done) {
-
+	
+	it('should add a contact and not throw an error',function(){
 		browser.get('http://localhost:8080');
-
-    element(by.model('contact.name')).sendKeys('Davos');
-		browser.sleep(3000);
-    element(by.model('contact.email')).sendKeys('davos@onionknig.ht');
-		element(by.id('submitNewContact')).click();
-		
-		
-//		browser.sleep(3,function(){
-			/* handy!*/
-
-//    		expect(newest.getText()).toEqual('Hello Julie!');
-//		});
-		
-		var contacts = element.all(by.repeater('contact in contacts'));
-		var newest = contacts[contacts.length-1];
-
-
-		
+		element(by.model('contact.name')).sendKeys('Davos Seaworth');
+		browser.sleep(1000);
+		element(by.model('contact.email')).sendKeys('davos@onionknig.ht');
+		browser.sleep(1000);
+		element(by.id('submitNewContact')).click();	
+		browser.sleep(1000);
+		// to do... assert all is well?
+	});
+	
+  it('should display the contacts name correctly', function(done) {		
+		element.all(by.repeater('contact in contacts'))
+		.then(function(contacts){
+			browser.sleep(200);
+			var newest = contacts[contacts.length-1];
+			var name = newest.element(by.tagName('h4'))
+				.element(by.className('contact-name'));
+			expect(name.getText()).toEqual("Davos Seaworth");
+			done();
+		});
   });
+	
+	it('should display the contacts avatar properly', function(done) {		
+		element.all(by.repeater('contact in contacts'))
+		.then(function(contacts){
+			browser.sleep(200);
+			var newest = contacts[contacts.length-1];
+			var avatar = newest.element(by.tagName('h4'))
+				.element(by.tagName('avatar'));
+			expect(avatar.getText()).toEqual("");
+			
+			done();
+		});
+  });
+	
+	it("should throw an error if I try to add the same contact again");
 });
