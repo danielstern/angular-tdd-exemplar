654193de13dac66046c3716422423205635f4b3d
Daniel Stern <daniel@danielstern.ca>
2015-05-18 20:28:15 -0400

    tests work. doesnt auto close but oh well.

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -3,24 +3,23 @@ describe('adding a contact', function() {
 	it('should add a contact and not throw an error',function(done){
 		browser.get('http://localhost:8080');
 		element(by.model('contact.name')).sendKeys('davos seaworth');
-		browser.sleep(1000);
 		element(by.model('contact.email')).sendKeys('davos@onionknig.ht');
-		browser.sleep(1000);
 		element(by.id('submitNewContact')).click();	
 		browser.sleep(1000);
-		// to do... assert all is well?
-		done();
+		element.all(by.repeater('contact in contacts'))
+		.then(function(contacts){
+			expect(contacts.length).toEqual(4);
+			done();	
+		});
 	});
 	
   it('should display the contacts name correctly', function(done) {		
 		element.all(by.repeater('contact in contacts'))
 		.then(function(contacts){
-			browser.sleep(200);
 			var newest = contacts[contacts.length-1];
 			var name = newest.element(by.tagName('h4'))
 				.element(by.className('contact-name'));
 			expect(name.getText()).toEqual("Davos Seaworth");
-			console.log("name displayed correctly.");
 			done();
 		});
   });
@@ -28,12 +27,10 @@ describe('adding a contact', function() {
 	it('should display the contacts avatar properly', function(done) {		
 		element.all(by.repeater('contact in contacts'))
 		.then(function(contacts){
-			browser.sleep(200);
 			var newest = contacts[contacts.length-1];
 			var avatar = newest.element(by.tagName('h4'))
 				.element(by.className('avatar'));
-			expect(avatar.getText()).toEqual("D");
-			
+			expect(avatar.getText()).toEqual("D");			
 		});
 		done();
   });
