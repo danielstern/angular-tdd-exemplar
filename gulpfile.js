d915e96efa7bc7bf4939e6693cfdca2efc15cb4e
Daniel Stern <daniel@danielstern.ca>
2015-06-02 06:44:50 -0400

    end lesson 2

--- gulpfile.js
+++ gulpfile.js
@@ -15,4 +15,20 @@ gulp.task('serve',function(){
 	
 	gulp.watch(['app/**/*.*'])
 		.on('change',browserSync.reload);
+});
+
+gulp.task('serve-test',function(){
+	browserSync.init({
+		notify:false,
+		port:8081,
+		server:{
+			baseDir:["test","app"],
+			routes:{
+				'/bower_components':'bower_components'
+			}
+		}
+	})
+	
+	gulp.watch(['app/**/*.*'])
+		.on('change',browserSync.reload);
 })
---
+++ test/main.spec.js
@@ -0,0 +1,5 @@
+describe("The Address Book App",function(){
+	it("should work",function(){
+		chai.assert.isArray([]);
+	})
+})
