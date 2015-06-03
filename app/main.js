a3ba11d6d96fcf227ce041677022b2081dddd931
Daniel Stern <daniel@danielstern.ca>
2015-06-03 07:34:20 -0400

    end lesson on module on inject

--- app/main.js
+++ app/main.js
@@ -1,1 +1,7 @@
-angular.module("AddressBook",[]);
+angular.module("AddressBook",[])
+.service("contactService",function($http){
+	this.contacts = [];
+	$http.get("http://localhost:9001/contacts",function(res){
+		console.log(res);
+	})
+});
--- test/main.spec.js
+++ test/main.spec.js
@@ -1,5 +1,16 @@
+var assert = chai.assert;
+var expect = chai.expect;
+
 describe("The Address Book App",function(){
-	it("should work",function(){
-		chai.assert.isArray([]);
+	describe("the contact service",function(){
+		it("should have a property contacts, an array",function(){
+			module('AddressBook');
+			inject(function($injector){
+				contactService = $injector.get("contactService");
+			});
+
+			expect(contactService.contacts).to.be.an('array');
+
+		})
 	})
 })
