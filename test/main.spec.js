2cf51b58365f6eeb27eaf189375aa20662927c6c
Daniel Stern <daniel@danielstern.ca>
2015-06-03 07:43:56 -0400

    end lesson on httpbackend

--- test/main.spec.js
+++ test/main.spec.js
@@ -3,14 +3,22 @@ var expect = chai.expect;
 
 describe("The Address Book App",function(){
 	describe("the contact service",function(){
-		it("should have a property contacts, an array",function(){
+		beforeEach(function(){
 			module('AddressBook');
 			inject(function($injector){
 				contactService = $injector.get("contactService");
+				$httpBackend = $injector.get("$httpBackend");
 			});
+		})
 
+		it("should have a property contacts, an array",function(){
 			expect(contactService.contacts).to.be.an('array');
+		});
 
+		it ("should call the backend",function(){
+			$httpBackend.expectGET("http://localhost:9001/contacts")
+				.respond(200,[]);
+			$httpBackend.flush();
 		})
 	})
 })
